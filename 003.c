b6c8f323ca193b8bd849f06f273204b1427d2121
Jerome Kuebler <jkuebler981@gmail.com>
2017-08-29 12:28:00 -0600

    003

---
+++ 003.c
@@ -0,0 +1,28 @@
+#include <math.h>
+#include "stdio.h"
+
+int prime(int num) {
+    int j;
+    int max_factor = (int) pow(num, 0.5);
+    for (j = 2; j < max_factor; j++){
+        if (num % j == 0){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int main() {
+
+    int max_prime = 0;
+    long long int factorize = 600851475143;
+    int max_factor = (int) pow(factorize, 0.5);
+    int i;
+    for (i = 2; i < max_factor; i++) {
+        if (prime(i) == 0 && factorize % i == 0) {
+                max_prime = i;
+        }
+    }
+    printf("%d", max_prime);
+    return 0;
+}
